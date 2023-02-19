6196a07d87ea61d35cd726ed4a51f4433611773b
niLPotential <niLPotential@proton.me>
2023-02-20 00:14:47 +0900

    Solve problem 001

--- 001/main.rs
+++ 001/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("hello, world");
+    let sum_of_threes = sum_below_thousand(3);
+    let sum_of_fives = sum_below_thousand(5);
+    let sum_of_fifteens = sum_below_thousand(15);
+    println!(
+        "Problem 001: {}",
+        sum_of_threes + sum_of_fives - sum_of_fifteens
+    );
+}
+
+fn sum_below_thousand(n: i32) -> i32 {
+    let div = (1000 - 1) / n;
+    return n * div * (div + 1) / 2;
 }
