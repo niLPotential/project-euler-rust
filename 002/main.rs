04ae469ac59c2f15484674d7096348d7652a9621
niLPotential <niLPotential@proton.me>
2023-02-20 11:57:43 +0900

    Add initial solution to problem 002

---
+++ 002/main.rs
@@ -0,0 +1,19 @@
+const MAX_LIMIT: i32 = 4000000;
+
+fn main() {
+    let mut sum_of_even_fibonacci = 0;
+
+    let mut first_fibonacci = 1;
+    let mut second_fibonacci = 2;
+
+    while second_fibonacci < MAX_LIMIT {
+        if second_fibonacci % 2 == 0 {
+            sum_of_even_fibonacci += second_fibonacci;
+        }
+
+        second_fibonacci += first_fibonacci;
+        first_fibonacci = second_fibonacci - first_fibonacci;
+    }
+
+    println!("Problem 002: {}", sum_of_even_fibonacci)
+}
