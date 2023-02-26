01648f539838c9ba8656d6f1103a8307943884a6
niLPotential <niLPotential@proton.me>
2023-02-26 21:30:43 +0900

    Optimize problem 004

--- 004/main.rs
+++ 004/main.rs
@@ -1,14 +1,24 @@
 fn main() {
     let mut largest_palindrome_product = 0;
-    for i in (100..1000).rev() {
-        for j in (100..1000).rev() {
-            let product = i * j;
+    for a in (100..1000).rev() {
+        let (mut b, db) = get_b(a);
+        while b >= a {
+            let product = a * b;
             if product > largest_palindrome_product && is_palindrome(product) {
-                largest_palindrome_product = product
+                largest_palindrome_product = product;
             }
+            b -= db;
         }
     }
     println!("Problem 004: {}", largest_palindrome_product);
+}
+
+fn get_b(a: u32) -> (u32, u32) {
+    if a % 11 == 0 {
+        return (999, 1);
+    } else {
+        return (990, 11);
+    }
 }
 
 fn is_palindrome(n: u32) -> bool {
