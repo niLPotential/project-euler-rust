bf50d0921e52d80b9caff995051142a9a597fd23
niLPotential <niLPotential@proton.me>
2023-11-30 23:32:24 +0900

    Solve problem 021

---
+++ 021/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let mut sum = 0;
+    for i in 2..=10000 {
+        if amicable(i) {
+            println!("{i}");
+            sum += i;
+        }
+    }
+    println!("Problem 021: {sum}");
+}
+
+fn amicable(n: u32) -> bool {
+    let potential_pair = sum_of_proper_divisors(n);
+    (n != potential_pair) && (n == sum_of_proper_divisors(potential_pair))
+}
+
+fn sum_of_proper_divisors(n: u32) -> u32 {
+    let mut sum = 1;
+    let mut i = 2;
+    while i * i < n {
+        if n % i == 0 {
+            sum += i + n / i;
+        }
+        i += 1;
+    }
+    sum
+}
